4ec15a28cdef1417e6bd329ed2765df58262f503
Devopsnikhil4 <138021468+Devopsnikhil4@users.noreply.github.com>
2023-06-29 16:15:08 +0530

    Auto push

--- 02-colour.sh
+++ 02-colour.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo welcome to devops
-echo -e "\e[41;31m I want to clear devops \e[0m"
-echo -e "\e[42;32m I want to clear devops \e[0m"
-echo -e "\e[43;33m I want to clear devops \e[0m"
-echo -e "\e[44;34m I want to clear devops \e[0m"
-echo -e "\e[45;35m I want to clear devops \e[0m"
+echo -e "\e[42;31m I want to clear devops \e[0m"
+echo -e "\e[43;32m I want to clear devops \e[0m"
+echo -e "\e[44;33m I want to clear devops \e[0m"
+echo -e "\e[45;34m I want to clear devops \e[0m"
+echo -e "\e[46;35m I want to clear devops \e[0m"
