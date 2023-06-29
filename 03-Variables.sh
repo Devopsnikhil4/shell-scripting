5cf66887aad143ccaef4de62677554c2e34bad04
Devopsnikhil4 <138021468+Devopsnikhil4@users.noreply.github.com>
2023-06-29 17:06:32 +0530

    Auto push

--- 03-Variables.sh
+++ 03-Variables.sh
@@ -4,9 +4,9 @@ b=20
 c=30
 d=40
 
-echo -e"\e[32m $a \e[0m"
-echo -e"\e[32m $b \e[0m"
-echo -e"\e[32m $c \e[0m"
-echo -e"\e[32m $d \e[0m"
+echo -e "\e[32m $a \e[0m"
+echo -e "\e[32m $b \e[0m"
+echo -e "\e[32m $c \e[0m"
+echo -e "\e[32m $d \e[0m"
 
 
