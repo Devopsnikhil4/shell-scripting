522477e85fb1c26a5f21d696932d4722f54297d6
Devopsnikhil4 <138021468+Devopsnikhil4@users.noreply.github.com>
2023-06-30 15:08:41 +0530

    Auto push

--- 04-dynamicVars.sh
+++ 04-dynamicVars.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-
-TODAYS_DATE=$(date +%F)
+Todays_date=30 jun 2023  #hard coding value make it consistant 
+TODAYS_DATE=$(date +%F)  #Always enclose expresions in paranthesis
 echo -e "Good morning and todays date is \e[32m $TODAYS_DATE \e[0m "
