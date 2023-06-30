71a0e9b9ba15421369f7d2e6f26d372111cd7ffd
Devopsnikhil4 <138021468+Devopsnikhil4@users.noreply.github.com>
2023-06-30 15:15:47 +0530

    Auto push

--- 04-dynamicVars.sh
+++ 04-dynamicVars.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 Todays_date=30 jun 2023  #hard coding value make it consistant 
 TODAYS_DATE=$(date +%F)  #Always enclose expresions in paranthesis
+NO_OF_SESSIONS=$(who | wc -l)
 echo -e "Good morning and todays date is \e[32m $TODAYS_DATE \e[0m "
+echo -e "Number of opened sessions : \e[32m $NO_OF_SESSIONS \e[0m "
