5ba1f7472fb565e616336ba3c8d10a2d3dfa5bb2
Devopsnikhil4 <138021468+Devopsnikhil4@users.noreply.github.com>
2023-06-30 16:01:10 +0530

    Auto push

---
+++ 07-commands.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+#Binary ( /bin , /sbin)
+#Aliases  (Aliases are shortcuts)
+#shell builtin commands
+#Fuctions #Fuctions are a set of command that can be written in a sequence and can be called n number of times as per
