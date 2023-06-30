4483bbd7d88c14f62fd39d4c9904090862b252c3
Devopsnikhil4 <138021468+Devopsnikhil4@users.noreply.github.com>
2023-06-30 15:33:00 +0530

    Auto push

--- 05-Specialvariables.sh
+++ 05-Specialvariables.sh
@@ -6,3 +6,11 @@
 
 echo $0 # $0 prints the script name your executing
 echo "Name of the script executed is $0"
+echo "Name of the trainer is $1"
+echo "Training Batch Number is $2"
+echo "current Topics is $3"
+
+# we can supply the vaules from the command line
+
+#bash scriptName.sh 100 200 300 (Like this you can supply  a maximum of 9 variables from the command line)
+#                     $1  $2  $3
