ac69175f596a8784dcb54b1fa0701c185633df87
Devopsnikhil4 <138021468+Devopsnikhil4@users.noreply.github.com>
2023-06-30 16:13:41 +0530

    Auto push

--- 07-commands.sh
+++ 07-commands.sh
@@ -3,4 +3,11 @@
 #Binary ( /bin , /sbin)
 #Aliases  (Aliases are shortcuts)
 #shell builtin commands
-#Fuctions #Fuctions are a set of command that can be written in a sequence and can be called n number of times as per
+#Fuctions #Fuctions are a set of command that can be written in a sequence and can be called n number of times as per required basics
+$sample() {
+echo "My name is Nikhil kumar"
+echo "My name is Bharath kumar"
+echo "My name is Nithya"
+echo "My name is Dharvik"
+}
+#Then you can just type sample
