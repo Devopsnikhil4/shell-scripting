2eb494ab39e282e8ef8f8b7dfd5c1fa23767a1ce
Devopsnikhil4 <138021468+Devopsnikhil4@users.noreply.github.com>
2023-07-01 14:46:04 +0530

    grammeer

--- Practice.sh
+++ Practice.sh
@@ -5,5 +5,5 @@ echo -e "Line3\nLine4"
 echo -e "\e[32m I am printing Green Color  \e[0m"
 echo -e "\e[32m I am printing green clour  \e[0m"
 echo -e "\e[45;32m I love you  \e[0m"
-# Nikhil=Nithya
-echo $Nikhil
+# a=10
+echo $a
