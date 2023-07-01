2ba2a33d16dd01510fa58777fb6577583217af55
Devopsnikhil4 <138021468+Devopsnikhil4@users.noreply.github.com>
2023-07-01 14:59:57 +0530

    grammeer

--- Practice.sh
+++ Practice.sh
@@ -6,5 +6,6 @@ echo -e "Line3\nLine4"
 echo -e "\e[45;32m I love you  \e[0m"
 Nikhil=Nithya
 echo -e "\e[33m $Nikhil  \e[0m"
-TODAY_DATE="1july2023"
+
 TODAY_DATE=$(date +%F)
+echo -e "\e[$TODAY_DATE \e[0m"
