862475cd363461e3eccda9f7acc07fa9d1e9eae0
Devopsnikhil4 <138021468+Devopsnikhil4@users.noreply.github.com>
2023-07-02 12:55:34 +0530

    grammeer

--- Practice.sh
+++ Practice.sh
@@ -1,6 +1,14 @@
 #!bin/bash
 
 sample() {
+    echo "I am a sample function with name sample"
+    echo "I am executing the sample function"
+    echo "Sample function is completed"
+}
+
+sample
+
+sample() {
     echo "My name is Nikhil kumar"
     echo "My name is Bharath kumar"
     echo "My name is Nithya"
