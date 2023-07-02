4f4ccd88a1d9dadc3c0f46cfef2913d3ddc87631
Devopsnikhil4 <138021468+Devopsnikhil4@users.noreply.github.com>
2023-07-02 13:12:56 +0530

    grammeer

--- Practice.sh
+++ Practice.sh
@@ -15,4 +15,11 @@ sample() {
     echo "My name is Dharvik"
 }
 
+ACTION=$1 
+
+if [ "$ACTION" == "start" ] ; then 
+        echo -e "\e[32m Starting RabbitMQ Service \e[0m"
+        exit 0  
+        
+
 sample
