c06fde20d76f2fe657d309955a4257cb2a28f005
Devopsnikhil4 <138021468+Devopsnikhil4@users.noreply.github.com>
2023-07-02 20:03:17 +0530

    grammeer

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -9,27 +9,27 @@ fi
 echo "Installing Nginx :"
 yum install nginx -y
 
-The frontend is the service in RobotShop to serve the web content over Nginx.
+# The frontend is the service in RobotShop to serve the web content over Nginx.
 
-Install Nginx.
+# Install Nginx.
 
-```
+
 # yum install nginx -y
 # systemctl enable nginx
 # systemctl start nginx
 
-```
 
-Let's download the HTDOCS content and deploy it under the Nginx path.
 
-```
+# Let's download the HTDOCS content and deploy it under the Nginx path.
+
+
 # curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
 
-```
 
-Deploy in Nginx Default Location.
 
-```
+# Deploy in Nginx Default Location.
+
+
 # cd /usr/share/nginx/html
 # rm -rf *
 # unzip /tmp/frontend.zip
@@ -38,7 +38,7 @@ Deploy in Nginx Default Location.
 # rm -rf frontend-main README.md
 # mv localhost.conf /etc/nginx/default.d/roboshop.conf
 
-```
 
-- Finally, restart the service once to effect the changes.
-- Now, you should be able to access the ROBOSHOP e-commerce webpage as shown below
+
+# - Finally, restart the service once to effect the changes.
+# - Now, you should be able to access the ROBOSHOP e-commerce webpage as shown below
