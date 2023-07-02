1bcbf8c28ad1ae066f84af01a95954f508d89360
Devopsnikhil4 <138021468+Devopsnikhil4@users.noreply.github.com>
2023-07-02 13:13:19 +0530

    grammeer

--- Practice.sh
+++ Practice.sh
@@ -1,25 +1,10 @@
 #!bin/bash
 
-sample() {
-    echo "I am a sample function with name sample"
-    echo "I am executing the sample function"
-    echo "Sample function is completed"
-}
-
-sample
-
-sample() {
-    echo "My name is Nikhil kumar"
-    echo "My name is Bharath kumar"
-    echo "My name is Nithya"
-    echo "My name is Dharvik"
-}
 
 ACTION=$1 
 
 if [ "$ACTION" == "start" ] ; then 
         echo -e "\e[32m Starting RabbitMQ Service \e[0m"
         exit 0  
-        
 
-sample
+
