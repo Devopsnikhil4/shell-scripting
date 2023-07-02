718d3714c763386cb956f453fb1d1871cfc49dbd
Devopsnikhil4 <138021468+Devopsnikhil4@users.noreply.github.com>
2023-07-02 12:53:31 +0530

    grammeer

--- Practice.sh
+++ Practice.sh
@@ -1,21 +1,5 @@
 #!bin/bash
 
-echo -e "Line1\t\Line2"
-echo -e "Line3\nLine4"
-
-echo -e "\e[45;32m I love you  \e[0m"
-Nikhil=Nithya
-echo -e "\e[33m $Nikhil  \e[0m"
-
-TODAY_DATE=$(date +%F)
-echo -e "\e[35m $TODAY_DATE \e[0m"
-
-
-echo "Name of the Trainer is $1"
-echo "Training Batch Number is $2"
-echo "Current Topics is $3"
-echo $#
-
 sample() {
     echo "I am a sample function with name sample"
     echo "I am executing the sample function"
