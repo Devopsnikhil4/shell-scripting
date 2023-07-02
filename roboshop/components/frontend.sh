3228bbaf17a76878a95a4e92c2a4ae496ca36cf3
Devopsnikhil4 <138021468+Devopsnikhil4@users.noreply.github.com>
2023-07-02 19:35:33 +0530

    grammeer

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -1,5 +1,11 @@
 #!bin/bash
 
+ID=$(id -u)
+if [ $ID -ne 0] ; then
+    echo -e  "e\[31m This script is expected to be run by a root user or with a sudo privilage \e[0m"
+    exit 1
+fi
+
 echo "Installing Nginx :"
 yum install nginx -y
 
