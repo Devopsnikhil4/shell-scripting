a92a26b50349d92c483147084ea8698cb381c86b
Devopsnikhil4 <138021468+Devopsnikhil4@users.noreply.github.com>
2023-07-03 15:17:37 +0530

    grammeer

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -23,3 +23,7 @@ echo -e "****** \e[35m $COMPONENT installation is Started \e[0m******"
 echo -n "Configuring the $COMPONENT repo :"
 curl --silent --location https://rpm.nodesource.com/setup_16.x | sudo bash - &>> LOGFILE
 stat $?
+
+echo -n "Instatalling Nodejs : "
+yum install nodejs -y &>> LOGFILE
+stat $?
