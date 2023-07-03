ab615dff58de2f6f0b9d7c32920c6cfa0549b042
Devopsnikhil4 <138021468+Devopsnikhil4@users.noreply.github.com>
2023-07-03 12:56:14 +0530

    grammeer

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -1,3 +1,23 @@
 #!bin/bash
 
-echo "I am mongodb "
+COMPONENT=mongodb
+LOGFILE="/tmp/${COMPONENT}.log"
+
+ID=$(id -u)
+if [ $ID -ne 0 ] ; then
+    echo -e "\e[31m This script is expected to be run by a root user or with a sudo privilage \e[0m"
+    exit 1
+fi
+
+stat() {
+if [ $1 -eq 0 ] ; then
+    echo -e "\e[32m success \e[0m"
+else
+    echo -e "\e[31m failure \e[0m"
+    exit 2
+fi
+}
+
+echo -n "Configuring the ${COMPONENT} repo :"
+curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/stans-robot-project/mongodb/main/mongo.repo
+stat $?
