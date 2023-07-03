5d30b0b46c7bb8ec9d770126ab01a0378f0ff2c8
Devopsnikhil4 <138021468+Devopsnikhil4@users.noreply.github.com>
2023-07-03 12:30:07 +0530

    grammeer

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -33,7 +33,7 @@ stat $?
 
 echo -n "EXtracting ${COMPONENT} component :"
 unzip /tmp/${COMPONENT}.zip &>> LOGFILE
-mv static/* .           &>> LOGFILE
+mv static/* . &>> LOGFILE
 rm -rf ${COMPONENT}-main README.md
 mv localhost.conf /etc/nginx/default.d/roboshop.conf
 stat $?
