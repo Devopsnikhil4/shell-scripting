1e526abc24dc6dc9931661b211abaaee9b83d446
Devopsnikhil4 <138021468+Devopsnikhil4@users.noreply.github.com>
2023-07-03 14:09:58 +0530

    grammeer

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -23,5 +23,10 @@ curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/stans
 stat $?
 
 echo -n "Installing $COMPONENT : "
-yum install -y mongodb-org &>> LOGFILE
+yum install -y $COMPONENT-org &>> LOGFILE
+stat $?
+
+echo -n "Starting the $COMPONENT : "
+systemctl enable mongod &>> LOGFILE
+systemctl start mongod  &>> LOGFILE
 stat $?
